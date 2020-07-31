9589b2a8867eb2ca68e0c84ddc1aebbed599d217
huangyf15 <1466879176@qq.com>
2020-07-31 13:08:05 +0800

    deleted: JavaScript

--- Javascript/lsn/java-tsinghua-1/arrayTest/src/ArrayTest.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-public class ArrayTest{
-  public static void main(String[] args){
-    System.out.println(Arrays.toString(args));
-    
-  }
-}
--- Javascript/lsn/java-tsinghua-1/helloWorld/src/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld{
-  public static void main(String[] args){
-    System.out.println("Hello World!");
-    System.out.println("I am learning java-tsinghua-1!");
-  }
-}
